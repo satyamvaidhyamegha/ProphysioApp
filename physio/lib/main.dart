73e9f4db10c9d30c72ccb1ca5a6a1ae56679f4bc
debz-g <debz.exe@gmail.com>
2022-10-10 14:26:35 +0530

    Added placeholder screens, removed redundant imports and added some minor resources for next section of screens.

--- physio/lib/main.dart
+++ physio/lib/main.dart
@@ -2,23 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:physio/constants/colors.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
-
+import 'package:physio/screens/Reports/placeholder_screen.dart';
 import 'package:physio/screens/onboarding/auth_screen3.dart';
-import 'package:physio/screens/onboarding/certification_screen.dart';
-import 'package:physio/screens/onboarding/helpus_helpyou.dart';
-import 'package:physio/screens/onboarding/professional_profile.dart';
-import 'package:physio/screens/onboarding/push_notifications.dart';
-import 'package:physio/screens/onboarding/signup_screen1.dart';
-
 import 'package:physio/screens/onboarding/otp_verification.dart';
+import 'package:physio/screens/onboarding/signup_screen1.dart';
 import 'package:physio/screens/onboarding/signup_screen2.dart';
-
 import 'package:physio/screens/onboarding/splash_screen2.dart';
 import 'package:physio/screens/onboarding/splash_screen.dart';
 import 'package:physio/screens/onboarding/profile_selection.dart';
 import 'package:physio/screens/onboarding/about_screen.dart';
 import 'package:physio/screens/onboarding/auth_screen.dart';
-import 'package:physio/screens/onboarding/pro_active';
 
 void main() {
   runApp(const MyApp());
@@ -49,16 +42,11 @@ class MyApp extends StatelessWidget {
         '/SplashPage2': (BuildContext context) => const SplashPage2(),
         '/selection': (BuildContext context) => const ProfileSelectionPage(),
         '/about': (BuildContext context) => const AboutPage(),
-
+        '/subs': (BuildContext context) => const OtpVerificationPage(),
         '/authPage': (BuildContext context) => const AuthPage(),
         '/authPage3': (BuildContext context) => const AuthPage3(),
         '/signupScreen1': (BuildContext context) => const SignupScreen1(),
         '/signupScreen2': (BuildContext context) => const SignupScreen2(),
-
-        //       '/login': (BuildContext context) => const LoginPage(),
-        //     '/detail1': (BuildContext context) => const DetailPage(),
-        //     '/welcomeuser': (BuildContext context) => const Welcomeuser(),
-        //'/check': (BuildContext context) =>  Routune1(),
       },
     );
   }
